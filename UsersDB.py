e41b9e668315874fb679181f909e270f6c26457b
janetaj <janetjesus75@gmail.com>
2020-07-01 01:25:52 -0400

    Delete UsersDB.py

--- UsersDB.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-connection = sqlite3.connect("users.db")
-print("Database opened successfully")
-cursor = connection.cursor()
-connection.execute("create table users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT  NULL, email TEXT UNIQUE, gender TEXT, contact TEXT, dob TEXT, profile_pic BLOB)")
-print("Table created successfully")
-connection.close()   
